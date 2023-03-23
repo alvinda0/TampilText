10b48436dde804786edf80f742b47152b1ad952f
alvinda0 <alvindashahrul0@gmail.com>
2023-03-23 14:24:42 +0800

    tampiltext

---
+++ app/src/main/java/com/example/tampiltext/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.tampiltext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var edtText :EditText
+    private lateinit var btnSubmit :Button
+    private lateinit var tvTampil : TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        edtText = findViewById(R.id.et_input)
+        btnSubmit = findViewById(R.id.btn_submit)
+        tvTampil = findViewById(R.id.tv_tampil)
+
+        btnSubmit.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+
+        if(btnSubmit.id == R.id.btn_submit){
+            val isiEditText = edtText.text.toString()
+            tvTampil.text = isiEditText
+        }
+    }
+}
